5ebad4ef6d3056193e301bfaf2489f3c7b1f6899
shripad_69 <shripadalgude@gmail.com>
2025-12-18 20:41:31 +0530

    introduced error

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const port = 8080;
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json());bibgyugygy
 
 app.post("/signup", async (req, res) => {
   const username = req.body.username;
